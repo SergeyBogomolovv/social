f6b052dab03c76a71f847652bde3eed373751c90
Sergey Bogomolov <bogomolovs693@gmail.com>
2024-11-14 09:32:45 +0300

    feat: add posts migration and create repositories

--- internal/posts/repository/posts.go
+++ internal/posts/repository/posts.go
@@ -12,27 +12,55 @@ type postsRepository struct {
 }
 
 func (r *postsRepository) GetMany(ctx context.Context, page int32, limit int32) ([]*models.Post, error) {
-	return nil, nil
+	query := `SELECT * FROM posts LIMIT $1 OFFSET $2`
+	posts := make([]*models.Post, 0)
+	err := r.db.SelectContext(ctx, &posts, query, limit, page)
+	if err != nil {
+		return nil, err
+	}
+	return posts, nil
 }
 
 func (r *postsRepository) GetById(ctx context.Context, id int64) (*models.Post, error) {
-	return nil, nil
+	query := `SELECT * FROM users WHERE post_id = $1`
+	post := new(models.Post)
+	err := r.db.GetContext(ctx, post, query, id)
+	if err != nil {
+		return nil, err
+	}
+	return post, nil
 }
 
 func (r *postsRepository) Create(ctx context.Context, dto models.CreatePostDto) (*models.Post, error) {
-	return nil, nil
+	query := `INSERT INTO posts (author_id, title, content) VALUES ($1, $2, $3) RETURNING *`
+	post := new(models.Post)
+	err := r.db.GetContext(ctx, post, query, dto.AuthorID, dto.Title, dto.Content)
+	if err != nil {
+		return nil, err
+	}
+	return post, nil
 }
 
 func (r *postsRepository) Update(ctx context.Context, dto models.UpdatePostDto) (*models.Post, error) {
-	return nil, nil
+	query := `UPDATE posts SET title = $1, content = $2 WHERE post_id = $3 RETURNING *`
+	post := new(models.Post)
+	err := r.db.GetContext(ctx, post, query, dto.Title, dto.Content, dto.ID)
+	if err != nil {
+		return nil, err
+	}
+	return post, nil
 }
 
 func (r *postsRepository) Delete(ctx context.Context, id int64) (*models.Post, error) {
-	return nil, nil
+	query := `DELETE FROM posts WHERE post_id = $1 RETURNING *`
+	post := new(models.Post)
+	err := r.db.GetContext(ctx, post, query, id)
+	if err != nil {
+		return nil, err
+	}
+	return post, nil
 }
 
 func NewPostsRepository(db *sqlx.DB) *postsRepository {
-	return &postsRepository{
-		db: db,
-	}
+	return &postsRepository{db: db}
 }
--- internal/users/repository/repository.go
+++ internal/users/repository/repository.go
@@ -12,23 +12,53 @@ type usersRepository struct {
 }
 
 func (r *usersRepository) GetMany(ctx context.Context, page int32, limit int32) ([]*models.User, error) {
-	return nil, nil
+	query := `SELECT * FROM users LIMIT $1 OFFSET $2`
+	users := make([]*models.User, 0)
+	err := r.db.SelectContext(ctx, &users, query, limit, page)
+	if err != nil {
+		return nil, err
+	}
+	return users, nil
 }
 
 func (r *usersRepository) GetById(ctx context.Context, id int64) (*models.User, error) {
-	return nil, nil
+	query := `SELECT * FROM users WHERE user_id = $1`
+	user := new(models.User)
+	err := r.db.GetContext(ctx, user, query, id)
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
 }
 
 func (r *usersRepository) Create(ctx context.Context, dto models.CreateUserDto) (*models.User, error) {
-	return nil, nil
+	query := `INSERT INTO users (username, password) VALUES ($1, $2) RETURNING *`
+	user := new(models.User)
+	err := r.db.GetContext(ctx, user, query, dto.Username, dto.Password)
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
 }
 
 func (r *usersRepository) Update(ctx context.Context, dto models.UpdateUserDto) (*models.User, error) {
-	return nil, nil
+	query := `UPDATE users SET username = $1 WHERE user_id = $2 RETURNING *`
+	user := new(models.User)
+	err := r.db.GetContext(ctx, user, query, dto.Username, dto.ID)
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
 }
 
 func (r *usersRepository) Delete(ctx context.Context, id int64) (*models.User, error) {
-	return nil, nil
+	query := `DELETE FROM users WHERE user_id = $1 RETURNING *`
+	user := new(models.User)
+	err := r.db.GetContext(ctx, user, query, id)
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
 }
 
 func NewUsersRepository(db *sqlx.DB) *usersRepository {
